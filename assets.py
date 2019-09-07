c684b870c42579c251a330cc873829fa074cfa4d
papodetrader <andrenvillela@hotmail.com>
2019-09-07 15:33:06 +0300

    update on assets

--- assets.py
+++ assets.py
@@ -12,10 +12,14 @@ asset = {
 
 
 assets = {
-    'Forex': [['EURUSD', 'EURAUD', 'EURSEK', 'USDCAD', 'USDJPY', 'GBPUSD', 'GBPCAD', 'AUDJPY', 'CHFJPY'], 'IDEALPRO', 'Forex'], 
-    'CFD': [['IBUS500', 'IBUST100', 'IBGB100'], 'SMART', 'CFD'],
+    'Forex': [['EURUSD', 'EURAUD', 'EURSEK', 'GBPJPY', 'GBPCAD', 'GBPUSD', 'USDCAD', 'USDJPY', 'AUDJPY', 
+            'AUDUSD', 'CHFZAR', 'CHFCNH', 'CHFCZK', 'CHFHUF', 'CHFHUF', 'CHFNOK', 'CHFPLN', 'CHFTRY',
+            'EURCNH', 'EURJPY', 'EURGBP', 'EURAUD', 'EURCAD', 'USDCNH', 'CNHJPY', 'CADJPY', 'GBPCNH', 
+            'GBPAUD', 'AUDCNH', 'CADCNH',], 'IDEALPRO', 'Forex'], 
+    'CFD': [['IBUS500', 'IBUST100', 'IBGB100', 'IBDE30', 'IBFR40', 'IBJP225', 'IBHK50', 'IBAU200', ], 'SMART', 'CFD'],
     'CMDTY': [['XAUUSD', 'XAGUSD'], 'SMART', 'CMDTY'], 
     'Future1': [['GC'], 'NYMEX', 'Future'],
     'Future2': [['ES', 'YM'], 'GLOBEX', 'Future'],
     'Stock': [['IBM', 'C', 'F'], 'NYSE', 'Stock']
 }
+
